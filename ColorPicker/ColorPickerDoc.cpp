7b420bce90052eae6a8dcac6f8fa8d13a96a50b9
Ice Karma <icekarma@gmail.com>
2024-03-04 16:52:39 -0800

    Project ColorPicker: Add new files ColorPickerDoc.{cpp,h}.

---
+++ ColorPicker/ColorPickerDoc.cpp
@@ -0,0 +1,38 @@
+#include "pch.h"
+
+#include "ColorPicker.h"
+#include "ColorPickerDoc.h"
+
+//#include <propkey.h>
+
+IMPLEMENT_DYNCREATE( CColorPickerDoc, CDocument )
+
+BEGIN_MESSAGE_MAP( CColorPickerDoc, CDocument )
+END_MESSAGE_MAP( )
+
+//CColorPickerDoc::CColorPickerDoc( ) noexcept {
+//	/*empty*/
+//}
+//
+//CColorPickerDoc::~CColorPickerDoc( ) {
+//	/*empty*/
+//}
+//
+//BOOL CColorPickerDoc::OnNewDocument( ) {
+//	if ( !CDocument::OnNewDocument( ) ) {
+//		return FALSE;
+//	}
+//
+//	// TODO: add reinitialization code here
+//	// (SDI documents will reuse this document)
+//
+//	return TRUE;
+//}
+//
+//void CColorPickerDoc::Serialize( CArchive& ar ) {
+//	if ( ar.IsStoring( ) ) {
+//		// TODO: add storing code here
+//	} else {
+//		// TODO: add loading code here
+//	}
+//}
---
+++ ColorPicker/ColorPickerDoc.h
@@ -0,0 +1,34 @@
+#pragma once
+
+class CColorPickerDoc:
+    public CDocument
+{
+
+    DECLARE_DYNCREATE( CColorPickerDoc )
+    DECLARE_MESSAGE_MAP( )
+
+    // Construction
+protected:
+
+    //CColorPickerDoc( ) noexcept;
+    //virtual ~CColorPickerDoc( );
+
+    // Attributes
+public:
+
+    // Operations
+public:
+
+    // Overrides
+public:
+
+    //virtual BOOL OnNewDocument( );
+    //virtual void Serialize( CArchive& ar );
+
+    // Implementation
+public:
+
+    // Generated message map functions
+protected:
+
+};
