59854ce28df8da4302f664f4f58d837bb842ccff
Ice Karma <icekarma@gmail.com>
2024-03-05 14:16:42 -0800

    Class CColorPickerDoc: Declutter class declaration.

--- ColorPicker/ColorPickerDoc.h
+++ ColorPicker/ColorPickerDoc.h
@@ -7,29 +7,13 @@ class CColorPickerDoc:
     DECLARE_DYNCREATE( CColorPickerDoc )
     DECLARE_MESSAGE_MAP( )
 
-    // Construction
 protected:
 
     CColorPickerDoc( ) noexcept { /*empty*/ }
 
     virtual ~CColorPickerDoc( ) { /*empty*/ }
 
-    // Attributes
-public:
-
-    // Operations
-public:
-
-    // Overrides
-public:
-
     virtual BOOL OnNewDocument( );
     //virtual void Serialize( CArchive& ar );
-
-    // Implementation
-public:
-
-    // Generated message map functions
-protected:
 
 };
