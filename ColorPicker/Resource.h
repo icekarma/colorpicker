807099d87f48a82cef8b61ee0f068921b0ad7c63
Ice Karma <icekarma@gmail.com>
2024-03-06 11:18:18 -0800

    Project ColorPicker: Dialog design and implementation continue.

--- ColorPicker/Resource.h
+++ ColorPicker/Resource.h
@@ -7,14 +7,21 @@
 #define IDR_MAINFRAME                   128
 #define IDR_ColorPickerTYPE             130
 #define IDD_CHILDVIEW                   310
+#define IDD_CHILDVIEW_2                 311
+#define IDC_LAB_L_CHANNEL               1000
+#define IDC_LAB_L_VALUE                 1001
+#define IDC_LAB_A_CHANNEL               1002
+#define IDC_LAB_A_VALUE                 1003
+#define IDC_LAB_B_CHANNEL               1004
+#define IDC_LAB_B_VALUE                 1005
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        313
+#define _APS_NEXT_RESOURCE_VALUE        312
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           311
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           312
 #endif
 #endif
