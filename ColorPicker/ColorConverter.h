eb196a9cb24f791460273bbe458f6deb9e138ff6
Ice Karma <icekarma@gmail.com>
2024-03-09 14:51:09 -0800

    File ColorConverter.h: Remove unnecessary #include.

--- ColorPicker/ColorConverter.h
+++ ColorPicker/ColorConverter.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "Color.h"
-#include "TransformsManager.h"
 
 LabColorValue  ConvertColor( SrgbColorValue const& color );
 SrgbColorValue ConvertColor( LabColorValue  const& color );
