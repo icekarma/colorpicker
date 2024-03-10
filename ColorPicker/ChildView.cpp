4215db81280e559061d4a5f8889ec47371c99288
Ice Karma <icekarma@gmail.com>
2024-03-10 11:09:14 -0700

    Method CChildView::PreCreateWindow: Use constants for stripping window styles we don't want.

--- ColorPicker/ChildView.cpp
+++ ColorPicker/ChildView.cpp
@@ -75,8 +75,8 @@ BOOL CChildView::PreCreateWindow( CREATESTRUCT& cs ) {
         return FALSE;
     }
 
-    cs.style     &= ~( WS_BORDER | WS_DLGFRAME | WS_THICKFRAME );
-    cs.dwExStyle &= ~( WS_EX_CLIENTEDGE );
+    cs.style     &= ~WindowStylesToRemove;
+    cs.dwExStyle &= ~ExtendedWindowStylesToRemove;
     cs.lpszClass  = AfxRegisterWndClass( CS_HREDRAW | CS_VREDRAW | CS_DBLCLKS, ::LoadCursor( nullptr, IDC_ARROW ), reinterpret_cast<HBRUSH>( COLOR_WINDOW + 1 ), nullptr );
 
     return TRUE;
