a3dcaf227327d1272b98fcf05e45edf253d03c56
Ice Karma <icekarma@gmail.com>
2024-03-12 17:07:24 -0700

    Class CColorPickerDoc: Start adding properties.

--- ColorPicker/ColorPickerDoc.h
+++ ColorPicker/ColorPickerDoc.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Color.h"
+#include "ColorConverter.h"
 
 class CColorPickerDoc:
     public CDocument
@@ -8,6 +9,42 @@ class CColorPickerDoc:
 
     DECLARE_DYNCREATE( CColorPickerDoc )
     DECLARE_MESSAGE_MAP( )
+
+public:
+
+    void SetColor( LabColorValue const& color ) {
+        LabColor  =               color;
+        SrgbColor = ConvertColor( color );
+    }
+
+    void SetColor( SrgbColorValue const& color ) {
+        LabColor  = ConvertColor( color );
+        SrgbColor =               color;
+    }
+
+    LabColorValue GetColorAsLab( ) const {
+        return LabColor;
+    }
+
+    SrgbColorValue GetColorAsSrgb( ) const {
+        return SrgbColor;
+    }
+
+    void SetChannels( int const x, int const y, int const z ) {
+        m_nChannelX = x;
+        m_nChannelY = y;
+        m_nChannelZ = z;
+    }
+
+    void GetChannels( int& x, int& y, int& z ) const {
+        x = m_nChannelX;
+        y = m_nChannelY;
+        z = m_nChannelZ;
+    }
+
+    int GetChannelX( ) const { return m_nChannelX; }
+    int GetChannelY( ) const { return m_nChannelY; }
+    int GetChannelZ( ) const { return m_nChannelZ; }
 
 protected:
 
@@ -17,12 +54,12 @@ protected:
 
     virtual BOOL OnNewDocument( );
 
-public:
-
-    // If you change LabColor, you must change SrgbColor too!
+    // Use SetColor to change these, because it keeps their values synchronized!
     LabColorValue  LabColor  { };
-
-    // If you change SrgbColor, you must change LabColor too!
     SrgbColorValue SrgbColor { };
+
+    int m_nChannelX { 0 };
+    int m_nChannelY { 1 };
+    int m_nChannelZ { 2 };
 
 };
