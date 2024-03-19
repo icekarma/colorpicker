1a16408221c49562882afda5ba9cee575a867bc7
Ice Karma <icekarma@gmail.com>
2024-03-18 19:40:35 -0700

    File ColorConverter.cpp: Use Triplet<T>.

--- ColorPicker/ColorConverter.cpp
+++ ColorPicker/ColorConverter.cpp
@@ -13,19 +13,19 @@ TransformsManager Transforms;
 //================================================
 
 SrgbColorValue ConvertColor( LabColorValue const& color ) {
-    FloatT  labValues[ImageLabValuesPerPixel];
-    uint8_t srgbValues[ImageSrgbBytesPerPixel];
+    Triplet<FloatT> labValues { };
+    uint8_t         srgbValues[ImageSrgbValuesPerPixel];
 
     color.GetChannelValues( labValues );
-    cmsDoTransform( Transforms.GetLabToSrgbTransform( ), labValues, srgbValues, 1 );
+    cmsDoTransform( Transforms.GetLabToSrgbTransform( ), labValues.data( ), srgbValues, 1 );
     return { srgbValues[2], srgbValues[1], srgbValues[0] };
 }
 
 LabColorValue ConvertColor( SrgbColorValue const& color ) {
-    uint8_t srgbValues[ImageSrgbBytesPerPixel];
-    FloatT  labValues[ImageLabValuesPerPixel];
+    Triplet<uint8_t> srgbValues { };
+    FloatT           labValues[ImageLabValuesPerPixel];
 
     color.GetChannelValues( srgbValues );
-    cmsDoTransform( Transforms.GetSrgbToLabTransform( ), srgbValues, labValues, 1 );
+    cmsDoTransform( Transforms.GetSrgbToLabTransform( ), srgbValues.data( ), labValues, 1 );
     return { labValues[0], labValues[1], labValues[2] };
 }
