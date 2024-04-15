49498dbab821bf81feb052bd6b586d2d9c93f1e4
Ice Karma <icekarma@gmail.com>
2024-04-15 13:31:28 -0700

    Class CSwatch: Don't bother doing anything if the color hasn't actually changed.

--- ColorPicker/Swatch.h
+++ ColorPicker/Swatch.h
@@ -11,18 +11,24 @@ public:
 
     CSwatch( CColorPickerDoc* pDoc, CStaticBrush* pStatic ):
         m_pDoc         { pDoc    },
-        m_pStaticBrush { pStatic }
+        m_pStaticBrush { pStatic },
+        m_color        { m_pDoc->GetSrgbColor( ) }
     {
-        Update( );
+        m_pStaticBrush->SetColor( m_color );
     }
 
     void Update( ) {
-        m_pStaticBrush->SetColor( m_pDoc->GetSrgbColor( ) );
+        if ( SrgbColor newColor { m_pDoc->GetSrgbColor( ) }; m_color != newColor ) {
+            m_color = newColor;
+            m_pStaticBrush->SetColor( m_color );
+        }
     }
 
 private:
 
     CColorPickerDoc const* m_pDoc;
     CStaticBrush*          m_pStaticBrush;
+
+    SrgbColor              m_color;
 
 };
