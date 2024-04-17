c26c47022e8e7b3ffa8070c45dfd3cca3ceefa3d
Ice Karma <icekarma@gmail.com>
2024-04-17 13:27:08 -0700

    Class CSwatch: Remove redundant code.
    Class CStaticBrush provides "de-duplication" of calls to SetColor.
    Also:
    * Rename constructor parameter pStatic to pStaticBrush.
    * Remove now-unnecessary member variable m_color.

--- ColorPicker/Swatch.h
+++ ColorPicker/Swatch.h
@@ -9,26 +9,20 @@ public:
 
     CSwatch( ) = delete;
 
-    CSwatch( CColorPickerDoc* pDoc, CStaticBrush* pStatic ):
-        m_pDoc         { pDoc    },
-        m_pStaticBrush { pStatic },
-        m_color        { m_pDoc->GetSrgbColor( ) }
+    CSwatch( CColorPickerDoc* pDoc, CStaticBrush* pStaticBrush ):
+        m_pDoc         { pDoc         },
+        m_pStaticBrush { pStaticBrush }
     {
-        m_pStaticBrush->SetColor( m_color );
+        Update( );
     }
 
     void Update( ) {
-        if ( SrgbColor newColor { m_pDoc->GetSrgbColor( ) }; m_color != newColor ) {
-            m_color = newColor;
-            m_pStaticBrush->SetColor( m_color );
-        }
+        m_pStaticBrush->SetColor( m_pDoc->GetSrgbColor( ) );
     }
 
 private:
 
     CColorPickerDoc const* m_pDoc;
     CStaticBrush*          m_pStaticBrush;
-
-    SrgbColor              m_color;
 
 };
